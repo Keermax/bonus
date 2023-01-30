7c20d5620af4589919c496902f4cfb1663aa211e
Your <you>
2023-01-30 10:46:05 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+    int amount = 1500;
+    int score = 12;
+    int bonus = 100;
+    if (amount < 1000)
+        System.out.println(score);
+    if (amount >= 1000){
+        System.out.println((amount / bonus) + score);}
+
+    }
+}
