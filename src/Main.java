713bfe5c2779d8a77d0e8894ba751786526c4c7a
Your <you>
2023-01-30 18:39:54 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 1350;
+        int amount = 150;
         int score = 12;
         int bonus = 1;
         if (amount < 1000) {
             System.out.println(score + " сумма бонусов ");
-        }
-        if (amount >= 1000) {
+        } else {
             System.out.println((score) + " счет до пополнения ");
             System.out.println((amount / 100 * bonus) + " сумма бонуса ");
             System.out.println((amount / 100 * bonus) + score + " счет после пополнения ");
